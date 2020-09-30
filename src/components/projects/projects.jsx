0d826fbd88cd470b36ef613cf77f09ebb06d1efb
akanshat <akanshat1999@gmail.com>
2020-09-30 21:51:20 +0530

    exp added

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -50,7 +50,21 @@ const Projects = () => {
           </div>
         </div>
       </div>
-      <div className="experience">EXPERIENCE</div>
+      <div className="experience-div">
+        <div className="experience">EXPERIENCE</div>
+        <div className="experience-content">
+          <h3>EnLume Inc.</h3>
+          <p>SDE Intern</p>
+          <span className="exp-content">
+            - R&D on overall understanding of Jitsi Components and workflow.
+            <br />
+            - Developed backend functionalities with Node.js.
+            <br />
+            - Developed swagger documentation for backend APIs.
+            <br />
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
